2753bad989a289a6076d96a1d7574a40f5317ce0
Enrique Ramos <enri46@gmail.com>
2015-07-06 00:11:40 +0200

    Gestión de errores y autoload de datos en DB

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,40 @@
-//GET /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+//Autoload - factoriza el códifo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(function(quiz){
+		if (quiz) {
+			req.quiz = quiz;
+			next();
+		}
+		else{
+			next(new Error('No existe quizId=' + quizId));
+		}
+	}).catch(function(error){ next(error); })
 };
 
-//GET /quizes/answer
+
+//GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes : quizes});
+	}).catch(function(error){ next(error); })
+};
+
+//GET /quizes/:id
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz : req.quiz});
+	})
+};
+
+//GET /quizes/:id/answer
 exports.answer = function(req, res){
-	if(req.query.respuesta === 'Roma' || req.query.respuesta === 'roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta || req.query.respuesta === req.quiz.respuesta.toLowerCase()){
+		resultado = 'Correcto';
 	}
-	else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
 //GET /author
