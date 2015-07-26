024788cc5e8334dddc4cb1bcab7e7b7bafd640e9
Enrique Ramos <enri46@gmail.com>
2015-07-26 22:47:26 +0200

    Creación de preguntas y validación de lo introducido en los campos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,12 +21,12 @@ exports.index = function(req, res) {
 		str = "%" + str.trim().replace(" ","%") + "%";
 		// Pasamos a lower las cadenas porque Postgres no es sensitive case
 		models.Quiz.findAll({where: ["lower(pregunta) like ?", str.toLowerCase()]}).then(function(quizes){
-			res.render('quizes/index.ejs', {quizes : quizes});
+			res.render('quizes/index.ejs', {quizes : quizes, errors: []});
 		}).catch(function(error){ next(error); })
 	}
 	else{
 		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index.ejs', {quizes : quizes});
+			res.render('quizes/index.ejs', {quizes : quizes, errors: []});
 		}).catch(function(error){ next(error); })
 	}
 };
@@ -34,7 +34,7 @@ exports.index = function(req, res) {
 //GET /quizes/:id
 exports.show = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz : req.quiz});
+		res.render('quizes/show', {quiz : req.quiz, errors: []});
 	})
 };
 
@@ -44,10 +44,39 @@ exports.answer = function(req, res){
 	if(req.query.respuesta === req.quiz.respuesta || req.query.respuesta === req.quiz.respuesta.toLowerCase()){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
+};
+
+//GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(	//crea objeto quiz
+			{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+
+	res.render('quizes/new', {quiz: quiz, errors: []});
+}
+
+//POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if(err){
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else{
+				// guarda en BD los campos pregunta y respuesta de quiz
+				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');
+				})	// Redirección a lista de preguntas
+			}
+		}
+	);
 };
 
 //GET /author
 exports.author = function(req,res){
-	res.render('author');
+	res.render('author', { errors: [] });
 }
